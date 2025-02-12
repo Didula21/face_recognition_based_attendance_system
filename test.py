3478ef62f712e9062fe24d07db37ae4176fd3cae
Didula21 <iamdidula159@gmail.com>
2025-02-12 11:05:18 +0530

    background

--- test.py
+++ test.py
@@ -4,6 +4,10 @@ import cv2
 import pickle
 import numpy as np
 import os
+import csv
+import time
+from datetime import datetime
+
 
 
 video = cv2.VideoCapture(0)
@@ -17,6 +21,8 @@ with open('data/faces_data.pkl','rb') as f:
 
 knn=KNeighborsClassifier(n_neighbors=5) 
 knn.fit(FACES,LABELS)
+
+imgBackground=cv2.imread("background.png")
 
 
 while True:
@@ -35,11 +41,14 @@ while True:
         crop_img =frame[y:y+h ,x:x+w, :]
         resized_img = cv2.resize(crop_img,(50,50)).flatten().reshape(1,-1)
         output=knn.predict(resized_img)
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),1)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 1)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (50, 50, 255), 2)
+        cv2.rectangle(frame, (x, y - 40), (x + w, y), (50, 50, 255), -1)
         cv2.putText(frame,str(output[0]),(x,y-15),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1)
         cv2.rectangle(frame, (x, y), (x + w, y + h), (50, 50, 255), 1)
-
-    cv2.imshow("Face Detection", frame)
+    
+    imgBackground[162:162+480,55:55+640] = frame
+    cv2.imshow("Face Detection", imgBackground)
 
     # Exit on pressing 'q'
     if cv2.waitKey(1) & 0xFF == ord('q'):
